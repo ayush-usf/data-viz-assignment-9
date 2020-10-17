e6a09fd0e055a19f3d41c7b40d6209ca189ea5fb
Ayush Arora <aarora7@dons.usfca.edu>
2020-10-16 17:25:08 -0700

    tooltip added

--- index2.js
+++ index2.js
@@ -1,5 +1,10 @@
+// Ref: https://bl.ocks.org/micahstubbs/7f360cc66abfa28b400b96bc75b8984e
+
 d3.json('link.json', createAdjacencyMatrix);
 function createAdjacencyMatrix(data) {
+
+    let tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
     const adjacencyMatrix = d3.adjacencyMatrixLayout();
     console.log('adjacencyMatrix', adjacencyMatrix);
     console.log('d3', d3);
@@ -21,17 +26,41 @@ function createAdjacencyMatrix(data) {
         .data(matrixData)
         .enter()
         .append('rect')
-        .attr('width', d => d.width)
-        .attr('height', d => d.height)
-        .attr('x', d => d.x)
-        .attr('y', d => d.y)
+        .attr('width', d => {
+            return d.width
+        })
+        .attr('height', d => {
+            return d.height
+        })
+        .attr('x', d => {
+            return d.x
+        })
+        .attr('y', d => {
+            return d.y
+        })
+        .on("mouseover",(d => mouseover(d.id)))
+        .on("mouseout",(d => mouseout()))
         .style('stroke', 'black')
         .style('stroke-width', '1px')
         .style('stroke-opacity', .1)
-        // .style('fill', d => someColors(d.source.group))
         .style('fill-opacity', d => d.weight * 0.8);
     d3.select('#adjacencyG')
         .call(adjacencyMatrix.xAxis);
     d3.select('#adjacencyG')
         .call(adjacencyMatrix.yAxis);
+
+    function mouseover(p) {
+        if(event.target.tagName === "rect" && event.target.style["fill-opacity"] != "0"){
+            let y_x = p.split("-")
+            tooltip
+                .style("left", event.pageX - 50 + "px")
+                .style("top", event.pageY - 70 + "px")
+                .style("display", "inline-block")
+                .html("X: "+y_x[1] +", Y: "+y_x[0]);
+        }
+    }
+
+    function mouseout() {
+        tooltip.style("display", "none");
+    }
 }
